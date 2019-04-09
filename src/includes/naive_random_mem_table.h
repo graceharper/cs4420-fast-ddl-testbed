01ecbe85338a677b4e5647ba5b9ea4c6882c3a86
Sai Gundlapalli <sai.g1996@gmail.com>
2019-04-08 20:19:14 -0400

    comments & docs (#1)
    * comments

--- src/includes/naive_random_mem_table.h
+++ src/includes/naive_random_mem_table.h
@@ -1,7 +1,10 @@
-//
-// Created by tejun on 3/23/2019.
-//
-
+/*
+** Created by tejun on 3/23/2019.
+**********************************
+** This header file defines the interface to the naive table implementation.
+** Tuple groups are not stored contiguously.
+** Instead, they are created when needed (when a previous tuple group doesn't have space for a new tuple).
+*/ 
 
 #pragma once
 
@@ -12,16 +15,7 @@
 #include <array>
 #include <memory>
 
-/**
- * Tuple groups are not stored contiguously.
- * Instead, they are created when needed (when a previous tuple group doesn't have space for a new tuple).
- *
- * On DDL operation (copy constructor):
- * - Locks the entire table
- * - Copies each tuple group into new tuple groups
- * - Does an atomic swap (across all tuple groups) after copying is done
- * - Unlocks the entire table
- */
+
 template<int NumAttr>
 class NaiveRandomMemTable {
 
@@ -31,6 +25,13 @@ public:
 
     ~NaiveRandomMemTable() = default;
 
+    /*
+    ** On DDL operation (copy constructor):
+    ** - Locks the entire table
+    ** - Copies each tuple group into new tuple groups
+    ** - Does an atomic swap (across all tuple groups) after copying is done
+    ** - Unlocks the entire table
+    */
     template<int PrevNumAttr>
     NaiveRandomMemTable(NaiveRandomMemTable<PrevNumAttr> &toCopy);
 
